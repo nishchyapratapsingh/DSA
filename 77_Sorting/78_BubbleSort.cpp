3472018c9d723dbe88ea2588fee4d123cd4a8dd6
nishchyapratapsingh <np1416@srmist.edu.in>
2025-04-23 13:33:34 +0530

    Fixed logic

--- 77_Sorting/78_BubbleSort.cpp
+++ 77_Sorting/78_BubbleSort.cpp
@@ -5,7 +5,7 @@ void bubbleSort(vector<int>& arr, int n)
     for(int i=0;i<n;i++){
         for (int j = 0; j<n-1 ;j++) {
           if (arr[j] > arr[j + 1]) {
-            swap(arr[i], arr[i + 1]);
+            swap(arr[j], arr[j + 1]);
           }
         }
     }
