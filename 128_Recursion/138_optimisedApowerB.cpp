dc07c111ee441e52075fc0f45d4fdc64b99f169c
nishchyapratapsingh <np1416@srmist.edu.in>
2025-04-23 13:18:27 +0530

    optimised approach for a^b

---
+++ 128_Recursion/138_optimisedApowerB.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+long long result(int a, int b)
+{
+    if (b == 0)
+    {
+        return 1;
+    }
+
+    if (b == 1)
+    {
+        return a;
+    }
+
+    long long ans = result(a, b / 2);
+
+    if (b % 2 == 0)
+    {
+        return ans * ans;
+    }
+    else
+    {
+        return a * ans * ans;
+    }
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << result(a, b);
+}
