349c161c15a88ca70113644faef58808b59a9ac5
nishchyapratapsingh <np1416@srmist.edu.in>
2025-04-23 13:38:18 +0530

    Optimisation

--- 77_Sorting/78_BubbleSort.cpp
+++ 77_Sorting/78_BubbleSort.cpp
@@ -1,12 +1,15 @@
-#include <bits/stdc++.h> 
-void bubbleSort(vector<int>& arr, int n)
-{   
-    // Write your code here
-    for(int i=0;i<n;i++){
-        for (int j = 0; j<n-1 ;j++) {
-          if (arr[j] > arr[j + 1]) {
-            swap(arr[j], arr[j + 1]);
-          }
-        }
+#include <bits/stdc++.h>
+void bubbleSort(vector<int> &arr, int n)
+{
+  // Write your code here
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      if (arr[j] > arr[j + 1])
+      {
+        swap(arr[j], arr[j + 1]);
+      }
     }
+  }
 }
