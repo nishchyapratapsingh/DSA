3d07d4aafc129fe1911b0329c27cc184cfc7445d
nishchyapratapsingh <np1416@srmist.edu.in>
2025-06-18 23:41:16 +0530

    Sliding window problems

---
+++ 160_SlidingWindow/161_MaxPointsFromKCards.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    int maxScore(vector<int> &cardPoints, int k)
+    {
+        int n = cardPoints.size();
+        int l = 0;
+        int currWinP = 0;
+        int totalScore = 0;
+        int minScore = INT_MAX;
+
+        for (int r = 0; r < n; r++)
+        {
+            currWinP += cardPoints[r];
+            if (r - l + 1 == n - k)
+                minScore = min(minScore, currWinP); // first window score
+            if (r - l + 1 > n - k)
+            {
+                currWinP -= cardPoints[l];
+                minScore = min(minScore, currWinP); // next window scores
+                l++;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            totalScore += cardPoints[i];
+        }
+
+        return totalScore - minScore;
+    }
+};
---
+++ 160_SlidingWindow/163_MaxOnesIII.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+    int longestOnes(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        // vector<bool> flip(n, false);
+        int zeros = 0;
+        int l = 0;
+        int maxOne = 0;
+        int nFlips = 0;
+
+        for (int r = 0; r < n; r++)
+        {
+            if (nums[r] == 0)
+            {
+                zeros++;
+                nFlips++;
+            }
+            while (nFlips > k)
+            {
+                if (nums[l] == 0)
+                {
+                    nFlips--;
+                    zeros--;
+                };
+                l++;
+            }
+            maxOne = max(maxOne, r - l + 1);
+        }
+        return maxOne;
+    }
+};
