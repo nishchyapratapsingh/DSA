4a8c3d52fad57dba6604a2377ed7fccc5bff05fb
nishchyapratapsingh <np1416@srmist.edu.in>
2025-07-30 21:27:29 +0530

    Bottom view of binary tree - just remove the condition for adding values in map

---
+++ 172_Binary Trees/180_BottomView.cpp
@@ -0,0 +1,48 @@
+class Solution
+{
+public:
+    // Function to return a list of nodes visible from the top view
+    // from left to right in Binary Tree.
+    vector<int> bottomView(Node *root)
+    {
+        // code here
+
+        if (!root)
+            return {};
+
+        queue<pair<Node *, int>> q;
+        unordered_map<int, Node *> map;
+
+        q.push({root, 0});
+        vector<int> keys;
+        vector<int> ans;
+
+        while (!q.empty())
+        {
+            Node *node = q.front().first;
+            int vlevel = q.front().second;
+            q.pop();
+
+            map[vlevel] = node; 
+
+            if (node->left != nullptr)
+                q.push({node->left, vlevel - 1});
+            if (node->right != nullptr)
+                q.push({node->right, vlevel + 1});
+        }
+
+        for (auto it = map.begin(); it != map.end(); it++)
+        {
+            keys.push_back(it->first);
+        }
+
+        sort(keys.begin(), keys.end());
+
+        for (int key : keys)
+        {
+            ans.push_back(map[key]->data);
+        }
+
+        return ans;
+    }
+};
