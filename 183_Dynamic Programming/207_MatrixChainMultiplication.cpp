e78c332ef2eb381158f8592bd5b979811243d2af
nishchyapratapsingh <np1416@srmist.edu.in>
2025-09-21 12:59:37 +0530

    Add bottom-up implementation for Matrix Chain Multiplication

--- 183_Dynamic Programming/207_MatrixChainMultiplication.cpp
+++ 183_Dynamic Programming/207_MatrixChainMultiplication.cpp
@@ -18,3 +18,25 @@ class Solution {
         return solve(dp, arr, 1, arr.size()-1);
     }
 };
+
+
+//Bottom Up
+#include <bits/stdc++.h> 
+
+int matrixMultiplication(vector<int> &arr, int n)
+{
+    // Write your code here.
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+    for (int len = 2; len < n; len++) {
+        for (int i = 0; i < n - len; i++) {
+            int j = i + len;
+            dp[i][j] = INT_MAX;
+
+            for (int k = i + 1; k < j; k++) {
+                int cost = dp[i][k] + dp[k][j] + arr[i] * arr[k] * arr[j];
+                dp[i][j] = min(dp[i][j], cost);
+            }
+        }
+    }
+    return dp[0][n-1];
+}
