115f5e5bf4032df60a7dc1ec41f762525c48d083
nishchyapratapsingh <np1416@srmist.edu.in>
2025-10-09 23:29:21 +0530

    Add memoization implementation for Evaluate Expression to True problem

--- 183_Dynamic Programming/212_EvaluateExpressionToTrue.cpp
+++ 183_Dynamic Programming/212_EvaluateExpressionToTrue.cpp
@@ -40,3 +40,48 @@ class Solution {
 };
 
 //memoisation
+// User function Template for C++
+class Solution {
+    int solve(string& s, int i, int j, bool isTrue, vector<vector<vector<int>>> &dp) {
+        if(i>j) {
+            return 0;
+        }
+        else if(i==j) {
+            if (isTrue) {
+                return s[i] == 'T';
+            }
+            else return s[i] == 'F';
+        }
+        if (dp[i][j][isTrue] != -1) {
+            return dp[i][j][isTrue];
+        }
+        int ans=0;
+        for (int k=i+1; k<j; k+=2) {
+            int lt = solve(s, i, k-1, true, dp);
+            int lf = solve(s, i, k-1, false, dp);
+            int rt = solve(s, k+1, j, true, dp);
+            int rf = solve(s, k+1, j, false, dp);
+            if (s[k]=='&') {
+                if (isTrue) ans += lt*rt;
+                else ans += lf*rf + lt*rf + lf*rt;
+            }
+            else if (s[k]=='|') {
+                if (isTrue) ans += lt*rt + lt*rf + lf*rt; 
+                else ans += rf*lf;
+            }
+            else {
+                if (isTrue) ans += lt*rf + lf*rt;
+                else ans += lt*rt + lf*rf;
+            }
+        }
+        return dp[i][j][isTrue] = ans;
+    }
+    
+  public:
+    int countWays(string &s) {
+        // code here
+        int n=s.length();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2, -1)));
+        return solve(s, 0, n-1, true, dp);
+    }
+};
