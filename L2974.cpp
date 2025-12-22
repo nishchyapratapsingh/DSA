66df5cf4144cfc2d5fbd98a7a902592565ea88b7
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2025-12-22 17:37:37 +0530

    Create L2974.cpp

---
+++ L2974.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < n; i+=2) {
+            swap(nums[i-1], nums[i]);
+        }
+        return nums;
+    }
+};
